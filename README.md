de806d2bc1ce2b6896b23b48074bb9f15deb8708
Jennifer Campbell <jennifercampbell28@hotmail.com>
2025-04-15 12:33:03 +0100

    Initial file setup complete. Tables created in seed.js file.

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+.env setup:
+
+1. In the terminal, run the following command to set up the databases:
+
+   `npm run setup-dbs`
+
+2. Create two .env files:
+   
+   `.env.test`
+   `.env.development`
+
+3. In the `.env.test` file, insert the following code:
+
+   `PGDATABASE=nc_news_test`
+
+4. In the `.env.development` file, insert the following code:
+
+   `PGDATABASE=nc_news`
--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -6,7 +6,7 @@ beforeAll(() => seed(data));
 afterAll(() => db.end());
 
 describe('seed', () => {
-  describe('topics table', () => {
+  describe.only('topics table', () => {
     test('topics table exists', () => {
       return db
         .query(
@@ -81,7 +81,7 @@ describe('seed', () => {
     });
   });
 
-  describe('users table', () => {
+  describe.only('users table', () => {
     test('users table exists', () => {
       return db
         .query(
@@ -155,7 +155,7 @@ describe('seed', () => {
     });
   });
 
-  describe('articles table', () => {
+  describe.only('articles table', () => {
     test('articles table exists', () => {
       return db
         .query(
@@ -363,7 +363,7 @@ describe('seed', () => {
     });
   });
 
-  describe('comments table', () => {
+  describe.only('comments table', () => {
     test('comments table exists', () => {
       return db
         .query(
@@ -543,9 +543,9 @@ describe('seed', () => {
   });
 });
 
-describe('data insertion', () => {
+describe.skip('data insertion', () => {
 
-  test('topics data has been inserted correctly', () => {
+  test.only('topics data has been inserted correctly', () => {
     return db.query(`SELECT * FROM topics;`).then(({ rows: topics }) => {
       expect(topics).toHaveLength(3);
       topics.forEach((topic) => {
