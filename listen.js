a453ab4c5cc0fd27cb308dc037b3da277b69359b
Jennifer Campbell <jennifercampbell28@hotmail.com>
2025-04-30 14:17:41 +0100

    Task 13: database connections and scripts for hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const {PORT = 9090} = process.env;
 
-app.listen(9090, () => {
-    console.log("Server is listening on port 9090...");
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
 })
