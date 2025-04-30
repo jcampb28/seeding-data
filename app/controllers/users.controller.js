47b0a78b4ebe9a2d591b57a4c06513a41b07868d
Jennifer Campbell <jennifercampbell28@hotmail.com>
2025-04-30 12:00:35 +0100

    Task 11: topic queries nad relevant error handling

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -4,7 +4,7 @@ const getUsers = (req, res, next) => {
     selectUsers()
     .then((users) => {
         res.status(200).send({users: users})
-    });
+    }).catch(next);
 };
 
 module.exports = {getUsers};
